0bc7835f581bf9d5126bd9389c256e85ce64934a
Axect <edeftg@gmail.com>
2018-01-25 17:57:03 +0900

    Hubble

--- chart/Autodiff.hs
+++ chart/Autodiff.hs
@@ -1,35 +1,54 @@
 module Autodiff where
 
-import GHC.Float
+data Dual = Dual Double Double deriving (Read, Show, Eq)
 
-data Dual a = Dual a a deriving (Eq, Read, Show)
+constantDual :: Double -> Dual
+-- ^ Lifts a constant number to the `Dual` type.
+constantDual x = Dual x 0
 
-instance Num a => Num (Dual a) where
-  (Dual u u') + (Dual v v') = Dual (u + v) (u' + v')
-  (Dual u u') * (Dual v v') = Dual (u * v) (u' * v + u * v')
-  (Dual u u') - (Dual v v') = Dual (u - v) (u' - v')
-  abs (Dual u u')           = Dual (abs u) (u' * signum(u))
-  signum (Dual u u')        = Dual (signum u) 0
-  fromInteger n             = Dual (fromInteger n) 0
+seedDual :: Double -> Double -> Dual
+-- ^ Creates a Dual number.
+seedDual x x' = Dual x x'
 
-instance Fractional a => Fractional (Dual a) where
-  (Dual u u') / (Dual v v') = Dual (u / v) ((u' * v - u * v') / (v*v))
-  fromRational n            = Dual (fromRational n) 0
+evaluateDual :: Dual -> Double
+-- ^ Used to evaluate a function.
+evaluateDual (Dual x _) = x
 
-instance (Eq a, Floating a) => Floating (Dual a) where
-  pi = Dual pi 0
-  exp (Dual u u') = Dual (exp u) (u' * exp u)
-  log (Dual u u') = Dual (log u) (u'/ u)
-  sqrt (Dual u u') = Dual (sqrt u) (u' / (2 * sqrt u))
-  sin (Dual u u') = Dual (sin u) (u' * cos u)
-  cos (Dual u u') = Dual (cos u) (-1 * u' * sin u)
-  tan (Dual u u') = Dual (tan u) (1 / (cos u)**2)
-  (Dual u u')**(Dual n 0) = Dual (u**n) (n * u**(n-1))
-  (Dual u 0)**(Dual v v') = Dual (u**v) (u**v * log v * v')
-  (Dual u u')**(Dual v v') = Dual (u**v) (v'**(log u) + (v * u') / u)
+differentiateDual :: Dual -> Double
+-- ^ Used to evaluate the derivative of a function.
+differentiateDual (Dual _ x') = x'
 
-takeDual :: Num a => Dual a -> a
-takeDual (Dual _ y) = y
+instance Num Dual where
+  (+) (Dual u u') (Dual v v') = Dual (u + v) (u' + v')
+  (*) (Dual u u') (Dual v v') = Dual (u * v) (u' * v + u * v')
+  (-) (Dual u u') (Dual v v') = Dual (u - v) (u' - v')
+  negate (Dual u u')          = Dual (negate u) (negate u')
+  abs (Dual u u')             = Dual (abs u) (u' * (signum u))
+  signum (Dual u u')          = Dual (signum u) 0
+  fromInteger n               = Dual (fromInteger n) 0
 
---dual2Double :: (Eq a, Floating a) => Dual a -> Double
---dual2Double t = float2Double $ takeDual t
+instance Fractional Dual where
+  (/) (Dual u u') (Dual v v') = Dual (u / v) ((u' * v - u * v') / v ** 2)
+  recip (Dual u u')           = Dual (recip u) (-1 * u' * (recip (u ** 2)))
+  fromRational n              = Dual (fromRational n) 0
+
+instance Floating Dual where
+  pi                              = Dual pi 0
+  exp (Dual u u')                 = Dual (exp u) (u' * exp u)
+  sqrt (Dual u u')                = Dual (sqrt u) (u' / (2 * sqrt u))
+  log (Dual u u')                 = Dual (log u) (u' / u)
+  sin (Dual u u')                 = Dual (sin u) (u' * cos u)
+  cos (Dual u u')                 = Dual (cos u) (- u' * sin u)
+  tan (Dual u u')                 = Dual (tan u) (1 / ((cos u) ** 2))
+  asin (Dual u u')                = Dual (asin u) (u' / (sqrt(1 - u ** 2)))
+  acos (Dual u u')                = Dual (acos u) (- u' / (sqrt(1 - u ** 2)))
+  atan (Dual u u')                = Dual (atan u) (u' / (1 + u ** 2))
+  sinh (Dual u u')                = Dual (sinh u) (u' * cosh u)
+  cosh (Dual u u')                = Dual (cosh u) (u' * sinh u)
+  tanh (Dual u u')                = Dual (tanh u) (u' * (1 - (tanh u) ** 2))
+  asinh (Dual u u')               = Dual (asinh u) (u' / (sqrt(1 + u ** 2)))
+  acosh (Dual u u')               = Dual (acosh u) (u' / (sqrt(u ** 2 - 1)))
+  atanh (Dual u u')               = Dual (atanh u) (u' / (1 - u ** 2))
+  (**) (Dual u u') (Dual v v')    = Dual (u ** v) (u ** v * (v' * (log u) + (v * u' / u)))
+  logBase (Dual u u') (Dual v v') = Dual (logBase u v) (((log v) * u' / u - (log u) * v' / v) / ((log u) ** 2))
+
--- chart/hubble.hs
+++ chart/hubble.hs
@@ -8,11 +8,11 @@ year = 60 * 60 * 24 * 365
 
 a t = t ** (2/3)
 
-hubble t = takeDual (a $ Dual t 1)
+hubble t = 1 / a t * differentiateDual (a $ Dual t 1)
 
 radius :: [Double] -> [(Double, Double)]
-radius ts = [(t, 1 / (hubble t)) | t <- ts]
+radius ts = [(t, 1 / (a t * hubble t)) | t <- ts]
 
 main = toFile def "hubble.png" $ do
   layout_title .= "Hubble Radius"
-  plot (line "radius" [radius [0,380000*year,10**(10)*year]])
+  plot (line "radius" [radius [1,380001*year..5*10**(10)*year]])
