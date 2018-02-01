9bb2bb0ce53e4f6805c94951ece3cff55ee96813
Axect <edeftg@gmail.com>
2018-02-01 15:22:49 +0900

    Determinant by Laplce

--- FP/Det/det2.hs
+++ FP/Det/det2.hs
@@ -11,15 +11,23 @@ main = do
 
 type Column = [Double]
 type Matrix = [Column]
+type Perms  = [Column]
 
+swap :: Eq a => a -> a -> ([a] -> [a])
 swap a b = map $
   \x -> if x==a then b
         else if x==b then a
         else x
-
 
 sgn :: Column -> Double
 sgn [] = 1.0
 sgn (1:xs) = sgn $ map (subtract 1) xs
 sgn (x:xs) = -sgn (swap 1 x (x:xs))
 
+genPerm :: Matrix -> Perms
+genPerm mat = permutations ([1..l]::Column) where l = fromIntegral $ length(mat)
+
+det :: Matrix -> Double
+det mat = sum [sgn sig * product [mat!!i!!(sig'!!i) | i <- [0..1]] | sig <- g, let sig' = map (floor) $ map (+(-1)) sig]
+  where g = genPerm mat -- [[Double]]
+        l = length (mat)
