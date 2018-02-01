705008adee20385e12775d4391133c3af0561924
Axect <edeftg@gmail.com>
2018-02-01 15:27:28 +0900

    Add Comment to Det2

--- FP/Det/det2.hs
+++ FP/Det/det2.hs
@@ -13,20 +13,24 @@ type Column = [Double]
 type Matrix = [Column]
 type Perms  = [Column]
 
+-- [1,2] [1,2,3] => [2,1,3]
 swap :: Eq a => a -> a -> ([a] -> [a])
 swap a b = map $
   \x -> if x==a then b
         else if x==b then a
         else x
 
+-- sgn [1,2,3] = +1, sgn [2,1,3] = -1
 sgn :: Column -> Double
 sgn [] = 1.0
 sgn (1:xs) = sgn $ map (subtract 1) xs
 sgn (x:xs) = -sgn (swap 1 x (x:xs))
 
+-- genPerm [[1,2],[3,4]] = [[1,2], [2,1]]
 genPerm :: Matrix -> Perms
 genPerm mat = permutations ([1..l]::Column) where l = fromIntegral $ length(mat)
 
+-- det A = \sum_{\sigma \in S_n} sgn(\sigma) * \product_{i=1}^n a_{i,\sigma_i}
 det :: Matrix -> Double
 det mat = sum [sgn sig * product [mat!!i!!(sig'!!i) | i <- [0..1]] | sig <- g, let sig' = map (floor) $ map (+(-1)) sig]
   where g = genPerm mat -- [[Double]]
