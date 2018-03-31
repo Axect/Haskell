a3453bf5658b86d4d70b495f9ce212009f981dd5
Axect <edeftg@gmail.com>
2018-03-31 19:45:12 +0900

    Beautiful hlint
    Former-commit-id: 0610568538c8c83afd55e6fb363cdec85c55fe62

--- Stack/quadratic/src/Quadratic.hs
+++ Stack/quadratic/src/Quadratic.hs
@@ -14,4 +14,18 @@ data Roots = Roots RootT RootT deriving Show
 roots :: Quadratic -> Roots
 roots (Quadratic 0 0 _) = error "Not a quadratic Polynomial"
 roots (Quadratic 0 b c) = let root = ( (-c) / b :+ 0) in Roots root root
+roots (Quadratic a b c) =
+    let discriminant = b * b - 4 * a * c in
+        rootsInternal (Quadratic a b c) discriminant
 
+rootsInternal :: Quadratic -> Double -> Roots
+rootsInternal q d
+    | d==0 = let r    = (-b q / 2.0 / a q)
+                 root = r :+ 0
+                   in Roots root root
+    | d<0 = Roots (realpart :+ complexpart) (realpart :+ (-complexpart))
+        where
+            plusd       = -d
+            twoa        = 2.0 * a q
+            complexpart = sqrt plusd / twoa
+            realpart    = - b q / twoa
