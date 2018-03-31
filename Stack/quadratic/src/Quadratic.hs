2266c9bb050268ff1203e2d7df5300d696147304
Axect <edeftg@gmail.com>
2018-03-31 20:00:54 +0900

    hlint!
    Former-commit-id: a54f98a952eaba85f790a3b0fc3a361c7d8854f4

--- Stack/quadratic/src/Quadratic.hs
+++ Stack/quadratic/src/Quadratic.hs
@@ -19,24 +19,22 @@ roots (Quadratic a b c) =
         rootsInternal (Quadratic a b c) discriminant
 
 rootsInternal :: Quadratic -> Double -> Roots
-rootsInternal q d
-    | d==0 = let r    = (-b q / 2.0 / a q)
-                 root = r :+ 0
-                   in Roots root root
-    | d<0 = Roots (realpart :+ complexpart) (realpart :+ (-complexpart))
+rootsInternal q d | d==0 = let r    = (-b q / 2.0 / a q)
+                               root = r :+ 0
+                                in Roots root root
+
+rootsInternal q d | d<0 = Roots (realpart :+ complexpart) (realpart :+ (-complexpart))
         where
             plusd       = -d
             twoa        = 2.0 * a q
             complexpart = sqrt plusd / twoa
             realpart    = - b q / twoa
-    | otherwise = Roots (root1 :+ 0) (root2 :+ 0)
-            where
-                plusd = -d
-                twoa = 2.0 * a q
-                dpart = (sqrt plus d) / twoa
-                prefix = - (b q) / twoa
-                root1 = prefix + dpart
-                root2 = prefix - dpart
 
-add :: Integer -> Integer -> Integer
-add p q = p + q
+rootsInternal q d  = Roots (root1 :+ 0) (root2 :+ 0)
+            where
+                plusd  = -d
+                twoa   = 2.0 * a q
+                dpart  = sqrt plusd / twoa
+                prefix = - b q / twoa
+                root1  = prefix + dpart
+                root2  = prefix - dpart
