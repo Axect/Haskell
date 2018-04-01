f51db33d166e8e9c9a3cbf3a4fa6fbd7eb856b29
Axect <edeftg@gmail.com>
2018-04-01 19:06:59 +0900

    Reverse
    Former-commit-id: 477267bde7bd775c3cbceee342c493c46b128565

---
+++ Stack/reverse/src/Reverse.hs
@@ -0,0 +1,16 @@
+module Reverse where
+
+import Prelude hiding (reverse)
+
+main :: IO ()
+main = do
+    let inp = [1..10]
+        rs  = reverse inp
+    putStrLn $ "Reverse of " ++ show inp ++ " is " ++ show rs
+
+
+reverse :: [a] -> [a]
+reverse xs = reverse' xs []
+    where
+        reverse' :: [a] -> [a] -> [a]
+        reverse' xs rs = foldl (flip (:)) rs xs
