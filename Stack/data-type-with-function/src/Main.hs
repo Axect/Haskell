0ba072002d26ef184e4c6d383422e61724795968
Axect <edeftg@gmail.com>
2018-04-04 11:32:03 +0900

    Brittany
    Former-commit-id: 32440bc4e80109bd618554a6d700ac12396e43c0

--- Stack/data-type-with-function/src/Main.hs
+++ Stack/data-type-with-function/src/Main.hs
@@ -1,27 +1,28 @@
-module Main where
+module Main
+where
 
 main :: IO ()
 main = do
-  let square x = x * x
-      sqrti = floor . sqrt . fromIntegral
+    let square x = x * x
+        sqrti = floor . sqrt . fromIntegral
 
-  let squareF = Func square
-      sqrtF   = Func sqrti
+    let squareF = Func square
+        sqrtF   = Func sqrti
 
-  let idF = compose squareF sqrtF
+    let idF = compose squareF sqrtF
 
-  putStrLn "Composing square and square root fnuctions"
-  putStrLn "is an identity. Applying a value should not change it"
-  print $ apply idF 3
+    putStrLn "Composing square and square root fnuctions"
+    putStrLn "is an identity. Applying a value should not change it"
+    print $ apply idF 3
 
-  let x     = Ghost
-      y     = Fix x
-      Fix z = y
+    let x     = Ghost
+        y     = Fix x
+        Fix z = y
 
-  putStrLn "Original Value is "
-  print x
-  putStrLn "After fixing, "
-  print z
+    putStrLn "Original Value is "
+    print x
+    putStrLn "After fixing, "
+    print z
 
 newtype Func a b = Func (a -> b)
 
