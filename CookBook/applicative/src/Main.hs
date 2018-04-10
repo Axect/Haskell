1bdcee8d2df29da09a6179d0d572802fc5511fce
Axect <edeftg@gmail.com>
2018-04-10 15:55:36 +0900

    Applicative Complete
    Former-commit-id: d47affe896537a25b4681f04934a0fd404802ee3

--- CookBook/applicative/src/Main.hs
+++ CookBook/applicative/src/Main.hs
@@ -5,10 +5,28 @@ import Control.Applicative
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "Multiply Lists"
+  putStrLn $ show $ multiplyLists [1..3] [11..13]
+  putStrLn ""
+  putStrLn "Tuple of Maybes"
+  putStrLn "Just 10 -> Just \"String\" -> Just (10, \"String\")"
+  putStrLn $ show $ tupleMaybe (pure 10) (Just "String")
+  putStrLn ""
+  putStrLn "Just 10 -> Nothing -> Nothing"
+  putStrLn $ (show :: Maybe (Int, String) -> String) $ tupleMaybe (Just 10) Nothing
+  putStrLn ""
+  putStrLn "Adding Either"
+  putStrLn "Right 10 -> Right 100 -> Right 110"
+  putStrLn $ (show :: Either String Int -> String) $ addEither (Right 10) (Right 100)
+  putStrLn "Left String -> Right 10 -> Left String"
+  putStrLn $ (show :: Either String Int -> String) $ addEither (Left "String") (Right 10)
+
 
 multiplyLists :: Num a => [a] -> [a] -> [a]
 multiplyLists xs ys = (*) <$> xs <*> ys
 
 tupleMaybe :: Maybe a -> Maybe b -> Maybe (a,b)
 tupleMaybe x y = (,) <$> x <*> y
+
+addEither :: Num a => Either c a -> Either c a -> Either c a
+addEither x y = pure (+) <*> x <*> y
